895768286e9c9be162a0724dbbcfa2694a2d9bbe
vikassharma2001 <112811509+vikassharma2001@users.noreply.github.com>
2024-02-06 23:25:12 +0530

    Final

---
+++ models/inventoryModel.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+
+const inventorySchema = new mongoose.Schema(
+  {
+    inventoryType: {
+      type: String,
+      required: [true, "inventory type require"],
+      enum: ["in", "out"],
+    },
+    bloodGroup: {
+      type: String,
+      required: [true, "blood group is require"],
+      enum: ["O+", "O-", "AB+", "AB-", "A+", "A-", "B+", "B-"],
+    },
+    quantity: {
+      type: Number,
+      require: [true, "blood quanity is require"],
+    },
+    email: {
+      type: String,
+      required: [true, "Donar Email is Required"],
+    },
+    hemail: {
+      type: String,
+      required: function () {
+        return this.inventoryType === "out";
+      },
+    },
+    organisation: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "users",
+      required: [true, "organisation is require"],
+    },
+    hospital: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "users",
+      required: function () {
+        return this.inventoryType === "out";
+      },
+    },
+    donar: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "users",
+      // required: function () {
+      //   return this.inventoryType === "in";
+      // },
+      required: [true, "donar is require"],
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Inventory", inventorySchema);
---
+++ readme.md
@@ -0,0 +1,7 @@
+A blood bank full stack project is a web application that helps to manage the operations of a blood bank.
+
+Tools & technologies used:-  JavaScript, Node.js, Express.js, React, MongoDB
+
+The objective of the project is to help blood banks manage their operations more efficiently and effectively. This application can manage tasks such as: Managing blood inventory, Tracking donor history, Managing organisation and hospitals. Basically this web application has 3 types of users donors, hospitals and organisations and a application administrator who can access database. Donors can access information like organisations they have interacted with. These are the organisations where these donors have either donated the blood or have taken out blood in time of need. The donor account also maintains the transaction of blood, donor has come across after the creation of account. Transaction data maintains the blood group, inventory type, Quantity and organisation email, hospital name, and timestamp.
+
+Live at :- https://blood-bank-72kq.onrender.com/login
